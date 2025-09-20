52ed8bf9ac0a991b4ad059c552444785695097bc
yusuf isbilir <dev.yusufisbilir@gmail.com>
2025-09-20 18:20:36 +0300

    move components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 import dynamic from "next/dynamic";
-import AuthGuard from "./components/AuthGuard";
+import AuthGuard from "../components/AuthGuard";
 
 const Dashboard = dynamic(() => import("./private/dashboard"), { ssr: false });
 
