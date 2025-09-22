d5f6812311bb6a00afc78d20d3fc580b5fa96ed2
yusuf isbilir <dev.yusufisbilir@gmail.com>
2025-09-22 20:40:17 +0300

    improve auth

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -27,6 +27,13 @@ export default function LoginPage() {
     setError("");
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/api/auth/callback`,
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
     });
     setLoading(false);
     if (error) {
